02129348ba091c8c1d78f5c63cf4b96bef5d0648
Abdallah Edris <abdallah.edris@iskraemeco.com>
2023-05-26 16:24:43 +0300

    make CreatedAt Nullable

---
+++ SoftwarePal/Migrations/20230526132228_add CreatedAt to blogs and parentCategory to Category.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoftwarePal.Migrations
+{
+    public partial class addCreatedAttoblogsandparentCategorytoCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParentName",
+                table: "Categories");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ParentCategoryId",
+                table: "Categories",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Blogs",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_ParentCategoryId",
+                table: "Categories",
+                column: "ParentCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Categories_ParentCategoryId",
+                table: "Categories",
+                column: "ParentCategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Categories_ParentCategoryId",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_ParentCategoryId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ParentCategoryId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Blogs");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ParentName",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
